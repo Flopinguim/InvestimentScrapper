58a1ae9389b42a2c6fcbaed4331b75194d5c1089
Pingui <08joaovitor01@gmail.com>
2024-01-02 01:28:22 -0300

    🎉 Commit inicial

---
+++ WebScrapper/Entities/Dividend.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebScrapper.Entities
+{
+    public class Dividend
+    {
+        public string CompanyName { get; set; }
+        public string Name { get; set; }
+        public string Segment { get; set; }
+        public string CNPJ { get; set; }
+        public double AverageDailytrading { get; set; }
+        public double LastYield { get; set; }
+        public double DividendYield { get; set; }
+        public double RentabilityPerMonth { get; set; }
+
+        public Dividend(string companyName, string name, string segment, string cnpj, double averageDailytrading, double lastYield, double dividendYield, double rentabilityPerMonth)
+        {
+            CompanyName =
+            Name =
+            Segment =
+            CNPJ = string.Empty;
+            AverageDailytrading =
+            LastYield =
+            DividendYield =
+            RentabilityPerMonth = 0d;
+        }
+    }
+ 
+}
---
+++ WebScrapper/WebScrapper.cs
@@ -0,0 +1,30 @@
+﻿using HtmlAgilityPack;
+
+namespace WebScrapper
+{
+    class WebScrapper
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Write the name of an Dividend");
+            string dividend = Console.ReadLine();
+
+            // Send get requent to site
+            string url = $"https://www.fundsexplorer.com.br/funds/{dividend.ToLower()}";
+            var httpClient = new HttpClient();
+            var html = httpClient.GetStringAsync(url).Result;
+            var htmlDocument = new HtmlDocument();
+            htmlDocument.LoadHtml(html);
+
+            // Get values
+            var dividendYieldElement = htmlDocument.DocumentNode.SelectNodes("//div[@class='indicators__box']");
+
+            foreach (var dividendhtml in dividendYieldElement)
+            {
+                var values = dividendhtml.Descendants("p").ToList();
+                Console.WriteLine(dividendhtml.InnerText.Replace("  ","").Replace("\t","").Replace("\n",""));
+                // Console.WriteLine(string.Concat(dividendhtml.InnerText.Where(Char.IsDigit)));
+            }
+        }
+    }
+}
