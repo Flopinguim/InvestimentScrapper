34720a61cb5508fe75dea9762d8c8d21da82a3b8
Pingui <08joaovitor01@gmail.com>
2024-03-10 20:09:55 -0300

    ✨ feat: Notificação

---
+++ UI/NotificationUI.xaml.cs
@@ -0,0 +1,20 @@
+﻿using MahApps.Metro.IconPacks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace UI
+{
+    /// <summary>
+    /// Interaction logic for NotificationUI.xaml
+    /// </summary>
+    public partial class NotificationUI : Window
+    {
+        public NotificationUI(string text, PackIconMaterialKind icon, Color background)
+        {
+            InitializeComponent();
+            txtNotification.Text = text;
+            borderNotification.Background = new SolidColorBrush(background);
+            iconNotification.Kind = icon;
+        }
+    }
+}
