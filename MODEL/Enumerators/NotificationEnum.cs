5a70fc320dd3c337284fa0c8e1d183d51614254a
Pingui <08joaovitor01@gmail.com>
2024-03-14 01:25:59 -0300

    🧱 ci: Criação do NotificationEnum

---
+++ MODEL/Enumerators/NotificationEnum.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MODEL.Enumerators
+{
+    public enum NotificationEnum
+    {
+        Informational,
+        Error,
+        Warning,
+        Success,
+    }
+}
